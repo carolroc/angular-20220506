c02fcc54e0f500ecde794eab6fd99dc941d6776a
carolroc <carolroc02@gmail.com>
2022-05-06 21:43:49 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GithubserviceService } from './githubservice.service';
-import { HttpClientModule } from '@angular/commom/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [GithubserviceService]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [GithubserviceService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/githubservice.service.ts
+++ src/app/githubservice.service.ts
@@ -1,23 +1,21 @@
-import { HttpClient } from '@angular/commun/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
-interface Repo{
+interface Repo {
   id: number;
   full_name: string;
 }
 
-@Injectable({
-  provideIn: 'root'
-})
+@Injectable()
 export class GithubserviceService {
   repos: Array<Repo> = [];
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  update(){
-    this.http.get<Array<Repo>>('https://api.github.com/users/carolroc/repos').subscribe(data => {
-      this.repos = data;
-    })
+  update() {
+    this.http
+      .get<Array<Repo>>('https://api.github.com/users/carolroc/repos')
+      .subscribe((data) => {
+        this.repos = data;
+      });
   }
-
 }
