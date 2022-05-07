7458d24dbea5932c869eb59cff5b61a5ebf33a71
carolroc <carolroc02@gmail.com>
2022-05-06 21:12:57 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GithubserviceService } from './githubservice.service';
+import { HttpClientModule } from '@angular/commom/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [GithubserviceService]
