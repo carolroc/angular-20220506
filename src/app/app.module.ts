1cefb15aaee87a9510efd3ac537e9ca8634733df
carolroc <carolroc02@gmail.com>
2022-05-06 21:47:44 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GithubserviceService } from './githubservice.service';
 import { HttpClientModule } from '@angular/common/http';
+import { BitcoinService } from './bitcoin.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
-  providers: [GithubserviceService],
+  providers: [GithubserviceService, BitcoinService],
 })
 export class AppModule {}
