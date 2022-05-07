aaf64a8c6579e8d7439caa19fa2aac14495ae08c
carolroc <carolroc02@gmail.com>
2022-05-06 21:50:49 -0300

    1 file modified

--- src/app/bitcoin.service.ts
+++ src/app/bitcoin.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 
+interface Response{
+  time: {updated: string};
+  bpi: {USD: {symbol: string; rate_float: number;};
+  EUR: {symbol: string; rate_float: number;}}
+
+  
+}
+
 @Injectable()
 export class BitcoinService {
 
